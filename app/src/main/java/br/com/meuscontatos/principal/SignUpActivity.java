d732499b409aad5583e195a50301bf304a8abda7
pablomaiden <pablomaiden@gmail.com>
2016-09-06 17:02:51 -0300

    Criando a tela de sign up fragment.

--- app/src/main/java/br/com/meuscontatos/principal/SignUpActivity.java
+++ app/src/main/java/br/com/meuscontatos/principal/SignUpActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import br.com.meuscontatos.principal.domain.Usuario;
+import br.com.meuscontatos.principal.service.Service;
+import io.realm.Realm;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText et_login;
+    private EditText et_senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+
+        et_login  = (EditText) findViewById(R.id.et_login);
+        et_login  = (EditText) findViewById(R.id.et_senha);
     }
 
     public void registrar(View view){
@@ -19,7 +30,9 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void autenticar(View view){
-
+        Usuario usuario = new Usuario();
+        Realm realm = Service.getInstace().getRealm(getApplicationContext());
+        Usuario usuario_= realm.where(Usuario.class).equalTo("usuario",et_login.getText().toString()).equalTo("senha",et_login.getText().toString()).findFirst();
     }
 
 }
