f1f8de2f639659ba916d23f7440b63d601aab2db
pablomaiden <pablomaiden@gmail.com>
2016-09-08 15:55:41 -0300

    camera

--- app/src/main/java/br/com/meuscontatos/principal/activity/CadastrarContatosActivity.java
+++ app/src/main/java/br/com/meuscontatos/principal/activity/CadastrarContatosActivity.java
@@ -1,10 +1,22 @@
 package br.com.meuscontatos.principal.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import br.com.meuscontatos.principal.R;
 import br.com.meuscontatos.principal.domain.Contato;
@@ -16,6 +28,11 @@ public class CadastrarContatosActivity extends AppCompatActivity {
     private EditText et_nome;
     private EditText et_email;
     private EditText et_senha;
+    public ImageView foto;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +42,10 @@ public class CadastrarContatosActivity extends AppCompatActivity {
         getSupportActionBar().setShowHideAnimationEnabled(true);
         getSupportActionBar().setTitle("Cadastro Contato");
 
-        et_nome  = (EditText) findViewById(R.id.et_nome);
-        et_email = (EditText) findViewById(R.id.et_email);
-        et_senha = (EditText) findViewById(R.id.et_senha);
+        et_nome  = (EditText)  findViewById(R.id.et_nome);
+        et_email = (EditText)  findViewById(R.id.et_email);
+        et_senha = (EditText)  findViewById(R.id.et_senha);
+        foto     = (ImageView) findViewById(R.id.foto);
     }
 
     @Override
@@ -70,4 +88,31 @@ public class CadastrarContatosActivity extends AppCompatActivity {
         return 0L;
     }
 
+    public void acionarCamera(View view){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+                foto.setImageBitmap(mImageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
