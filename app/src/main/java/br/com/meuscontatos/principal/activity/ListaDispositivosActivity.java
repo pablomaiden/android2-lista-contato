7a44397fea0408fddc98bbd823c8aa2e8bba68ef
patricknasc <patrick.nascimento@gmail.com>
2016-09-21 23:22:53 -0300

    A ListaDispositivosActivity agora esta recuperando a lista de dispositivos previamente pareados com o dispositivo cliente do app e lista o nome e o mac do device em uma lista simples do android.

--- app/src/main/java/br/com/meuscontatos/principal/activity/ListaDispositivosActivity.java
+++ app/src/main/java/br/com/meuscontatos/principal/activity/ListaDispositivosActivity.java
@@ -1,7 +1,12 @@
 package br.com.meuscontatos.principal.activity;
 
 import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.Set;
 
 /**
  * Created by patricknasc on 21/09/16.
@@ -9,9 +14,28 @@ import android.os.Bundle;
 
 public class ListaDispositivosActivity extends ListActivity {
 
+    private BluetoothAdapter myBluetoothAdapter = null;
+    private String MAC_ADDRESS = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> arrayBluetoothAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Set<BluetoothDevice> pairedDevices = myBluetoothAdapter.getBondedDevices();
+
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice device : pairedDevices){
+                String deviceName = device.getName();
+                String deviceMac = device.getAddress();
+                arrayBluetoothAdapter.add(deviceName + "\n" + deviceMac);
+            }
+        }
+        setListAdapter(arrayBluetoothAdapter);
+
 
     }
 }
