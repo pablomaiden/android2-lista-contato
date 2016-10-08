47d044e80eaad6b0ea25a7607ee55b6763e7d2b4
pablomaiden <pablomaiden@gmail.com>
2016-10-08 11:55:37 -0300

    Chat

--- app/src/main/java/br/com/meuscontatos/principal/activity/ChatActivity.java
+++ app/src/main/java/br/com/meuscontatos/principal/activity/ChatActivity.java
@@ -5,6 +5,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+
 import br.com.meuscontatos.principal.R;
 import br.com.meuscontatos.principal.domain.Contato;
 import br.com.meuscontatos.principal.service.Service;
@@ -13,6 +17,9 @@ import io.realm.Realm;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private EditText et_send;
+    private EditText et_chat;
+    private Button btn_send;
     private Contato contato;
 
     @Override
@@ -21,9 +28,13 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.chatbubble);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setShowHideAnimationEnabled(true);
-
         recuperarContato();
         getSupportActionBar().setTitle(contato.getNome());
+
+        et_send  = (EditText) findViewById(R.id.et_send);
+        et_chat  = (EditText) findViewById(R.id.et_chat);
+        btn_send = (Button)   findViewById(R.id.btn_send);
+
     }
 
     public void recuperarContato(){
@@ -36,6 +47,12 @@ public class ChatActivity extends AppCompatActivity {
     public Contato getContato(Long id){
         Realm realm = Service.getInstace().getRealm(getApplicationContext());
         return realm.where(Contato.class).equalTo("id",id).findFirst();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
     }
 
 }
