8400012685c43377c630e2f8fbd07e902645e8d6
patricknasc <patrick.nascimento@gmail.com>
2016-10-12 19:41:56 -0300

    WIP - Mapas redondo [Não mexer mais aqui!] - O Marcador agora segue o usuário logado.

--- app/src/main/java/br/com/meuscontatos/principal/fragment/MapaFragment.java
+++ app/src/main/java/br/com/meuscontatos/principal/fragment/MapaFragment.java
@@ -1,8 +1,19 @@
 package br.com.meuscontatos.principal.fragment;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -10,17 +21,31 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import br.com.meuscontatos.principal.R;
 
 public class MapaFragment extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final int MAP_PERMISSION_ACCESS_COURSE_LOCATION = 9999;
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    private Marker yourMarker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mapa_tab);
+        if(googleServicesAvailable()){
+            Toast.makeText(this, "Carregando...", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.mapa_tab);
+        }
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
 
         GoogleMapOptions options = new GoogleMapOptions();
         options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
@@ -29,10 +54,59 @@ public class MapaFragment extends FragmentActivity implements OnMapReadyCallback
                 .tiltGesturesEnabled(false);
     }
 
+
+    public boolean googleServicesAvailable() {
+            GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+            int isAvailable = api.isGooglePlayServicesAvailable(this);
+            if(isAvailable == ConnectionResult.SUCCESS){
+                    return true;
+                } else if(api.isUserResolvableError(isAvailable)){ //Deu erro mas pode ser consertado pelo usuário
+                    Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+                    dialog.show();
+                } else {
+                    Toast.makeText(this, "Não foi possível se conectar ao Play Services", Toast.LENGTH_LONG).show();
+                }
+            return false;
+    }
+
+
+
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-15.8728144,-48.0166341);
-        map.addMarker(new MarkerOptions().position(sydney).title("Brasília"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = map;
+
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions( this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MAP_PERMISSION_ACCESS_COURSE_LOCATION);
+            } else {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 1, new LocationListener(){
+
+
+                   @Override
+                    public void onLocationChanged(Location location) {
+
+                       if(yourMarker != null) { yourMarker.remove(); }
+                            LatLng newPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions newMarkerOptions = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Você");
+                            yourMarker = mMap.addMarker(newMarkerOptions);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newPosition, 30));
+
+                       }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+                    @Override
+                    public void onProviderEnabled(String provider) { }
+
+                    @Override
+                    public void onProviderDisabled(String provider) { }
+                });
+            }
+
+            LatLng iesbSul = new LatLng(-15.7571194, -47.8788442);
+            mMap.addMarker(new MarkerOptions().position(iesbSul).title("IESB Sul"));
     }
 }
